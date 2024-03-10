902d746a2455745bffcbf8c00e262cf1ec2f9642
WillKirkmanM <WillKirkmanMoller@gmail.com>
2024-03-10 10:01:02 +0000

    Feat(Editor): Start of Platofrm Processes

--- src/Editor.zig
+++ src/Editor.zig
@@ -1,4 +1,8 @@
-const run = @import("Run.zig");
-pub fn editor() anyerror !void{
-    try run.run();
+const std = @import("std");
+
+const windows = @import("./Windows.zig");
+
+pub fn getCurrentEditor() {
+    const processes = windows.getProcesses();
+    std.debug.print(processes, args: anytype);
 }
